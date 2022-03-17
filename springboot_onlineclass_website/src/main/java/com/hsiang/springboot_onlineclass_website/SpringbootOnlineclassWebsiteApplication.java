eacf934b7404c28b084621ae484f82aa79aed3f4
Hsiang <cs51068@gmail.com>
2022-03-17 11:07:38 +0800

    springboot download

---
+++ springboot_onlineclass_website/src/main/java/com/hsiang/springboot_onlineclass_website/SpringbootOnlineclassWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.hsiang.springboot_onlineclass_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOnlineclassWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOnlineclassWebsiteApplication.class, args);
+	}
+
+}
---
+++ springboot_onlineclass_website/src/test/java/com/hsiang/springboot_onlineclass_website/SpringbootOnlineclassWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsiang.springboot_onlineclass_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOnlineclassWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
